c013c47f11bddcd5a5781e9280b47bf73365aff0
Garrett LeSage <garrett@lesage.us>
2013-10-26 20:42:47 +0100

    removed source-old

--- source-old/javascripts/application.js
+++
@@ -1,7 +0,0 @@
-//= require vendor/jquery.js
-//= require_tree ./vendor
-//= require_tree ./lib
-
-/* Turn off parts of bootstrap that you do not need in
- * _bootstrap-includes.js */
-//= require _bootstrap-includes
