7cdab4544fc122d3c966331344db1c719c09614b
Garrett LeSage <garrett@lesage.us>
2015-08-05 17:18:03 +0200

    added summaries, site URL, and Gluster

--- fetch-dashboard.rb
+++ fetch-dashboard.rb
@@ -12,19 +12,32 @@ base_dir = 'data/dashboard'
 sites = [
   {
     name: 'oVirt',
+    summary: 'A virtualization platform with an easy-to-use web interface that manages virtual machines, storage, and virtualized networks. oVirt is open source, freely available, and powered by KVM on Linux.',
     dashboard: 'http://www.ovirt.org/stats/',
     url: 'http://www.ovirt.org/stats/data/json/',
+    site: 'https://ovirt.org/',
     stats: %w(irc its mls scm)
   }, {
     name: 'RDO',
+    summary: 'A community of people using and deploying OpenStack® on Red Hat Enterprise Linux, Fedora, and other Linux distributions based on these.',
     dashboard: 'https://www.rdoproject.org/stats/',
     url: 'https://www.rdoproject.org/stats/data/json/',
+    site: 'https://rdoproject.org/',
     stats: %w(irc its mediawiki mls qaforums scm)
   }, {
     name: 'ManageIQ',
+    summary: "Control applications and workloads across cloud platforms, from one end of a virtual machine's lifecycle to another.",
     dashboard: 'http://projects.bitergia.com/redhat-manageiq-dashboard/browser/',
     url: 'http://projects.bitergia.com/redhat-manageiq-dashboard/browser/data/json/',
+    site: 'https://manageiq.org/',
     stats: %w(irc its scm scr qaforums downloads)
+  }, {
+    name: 'Gluster',
+    summary: 'GlusterFS is a scalable network filesystem. Using common off-the-shelf hardware, you can create large, distributed storage solutions for media streaming, data analysis, and other data- and bandwidth-intensive tasks.',
+    dashboard: 'http://projects.bitergia.com/redhat-glusterfs-dashboard/browser/',
+    url: 'http://projects.bitergia.com/redhat-glusterfs-dashboard/browser/data/json/',
+    site: 'https://gluster.org/',
+    stats: %w(irc its scm mls downloads)
   }
 ]
 
