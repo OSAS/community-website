0b849beb06ca868f52a2f8ba26d066749cb212ca
Garrett LeSage <garrett@lesage.us>
2016-05-24 15:57:00 +0200

    upgraded to slop 4

--- create-post.rb
+++ create-post.rb
@@ -12,21 +12,25 @@ require 'slop'
 # Disable ActiveSupport's UTF-8 warning
 I18n.enforce_available_locales = false
 
-banner = 'Usage: ' + __FILE__ + ' [options] "Blog title here."'
+slop_opts = {
+  help: true,
+  ignore_case: true,
+  optional_arguments: true,
+  banner: 'Usage: ' + __FILE__ + ' [options] "Blog title here."'
+}
 
 # Command line options are parsed using Slop
-opts = Slop.parse!(help: true, ignore_case: true, banner: banner) do
-  on 'a', 'author', "Author name", argument: :required, default: ENV['USER']
-  on 'd', 'date', "Custom datetime", argument: :required, default: Time.now.strftime('%F %T %Z')
-  on 'e', 'editor', "Custom editor", argument: :required, default: ENV['EDITOR']
-  on 't', 'tags', "Comma-separated list of tags", argument: :required
-  on 'n', 'no-browser', "Disable browser"
+opts = Slop.parse(slop_opts) do |o|
+  o.string '-a', '--author', 'Author name', argument: :required, default: ENV['USER']
+  o.string '-d', '--date', 'Custom datetime', argument: :required, default: Time.now.strftime('%F %T %Z')
+  o.string '-e', '--editor', 'Custom editor', argument: :required, default: ENV['EDITOR']
+  o.on '-t', '--tags', 'Comma-separated list of tags', argument: :required
+  o.on '-n', '--no-browser', 'Disable browser'
 end
 
-# Since we're using 'parse!', Slop removes the flags from ARGV,
+# Slop removes the flags from ARGV and places them in opts.arguments,
 # so we're left with the title (either as a string or in parts)
-title = ARGV.join(' ').strip
-
+title = opts.arguments.join(' ').strip
 
 # Simply display usage and exit if there's no title provided
 if title.empty?
@@ -34,9 +38,8 @@ if title.empty?
   exit
 end
 
-
 # Normalize CSV
-tags = opts[:tags] ? opts[:tags].gsub(/,\s*/, ", ") : nil
+tags = opts[:tags] ? opts[:tags].gsub(/,\s*/, ', ') : nil
 
 # Frontmatter
 meta = {
@@ -48,10 +51,9 @@ meta = {
 
 content = "#{meta.to_yaml}---\n\n\n"
 
-
 # File values
 root = File.dirname(__FILE__)
-ext = "html.md"
+ext = 'html.md'
 path = "#{root}/source/blog"
 file = "#{meta['date'].to_date}-#{title.parameterize}"
 file_full = "#{path}/#{file}.#{ext}"
@@ -61,7 +63,6 @@ date_parts = 2
 date_urlfrag = meta['date'].to_date.to_s.split(/-/).take(date_parts).join('/')
 file_urlfrag = "#{date_urlfrag}/#{title.parameterize}"
 url = "http://0.0.0.0:4567/blog/#{file_urlfrag}"
-
 
 # Save the file
 File.write file_full, content
@@ -73,8 +74,8 @@ if opts[:editor]
   exec opts[:editor], file_full
 else
   # Fall back to xdg-open (or platform equiv)
-  Launchy.open "#{file_full}"
+  Launchy.open file_full.to_s
 end
 
 # View file in a webpage
-Launchy.open url unless opts["no-browser"]
+Launchy.open url unless opts['no-browser']
