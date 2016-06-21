a64e8b6b3ba6fc764d0ddc5fb923bd818bc5ae4c
Marc Dequènes (Duck) <duck@redhat.com>
2016-06-21 23:38:16 +0900

    make fetch-dashboard script configurable

--- fetch-dashboard.rb
+++ fetch-dashboard.rb
@@ -1,45 +1,21 @@
 #!/usr/bin/ruby
 
+# Dependencies: rubygem-activesupport
+
 require 'time'
 require 'fileutils'
 require 'net/http'
 require 'uri'
 require 'io/console'
 require 'json'
+require 'yaml'
+require 'active_support'
+require 'active_support/core_ext'
+
 
 base_dir = 'data/dashboard'
 
-sites = [
-  {
-    name: 'oVirt',
-    summary: 'A virtualization platform with an easy-to-use web interface that manages virtual machines, storage, and virtualized networks. oVirt is open source, freely available, and powered by KVM on Linux.',
-    dashboard: 'http://projects.bitergia.com/redhat-ovirt-dashboard/browser/',
-    url: 'http://projects.bitergia.com/redhat-ovirt-dashboard/browser/data/json/',
-    site: 'https://ovirt.org/',
-    stats: %w(irc its mls scm)
-  }, {
-    name: 'RDO',
-    summary: 'A community of people using and deploying OpenStack® on Red Hat Enterprise Linux, Fedora, and other Linux distributions based on these.',
-    dashboard: 'http://projects.bitergia.com/redhat-rdo-dashboard/browser/',
-    url: 'http://projects.bitergia.com/redhat-rdo-dashboard/browser/data/json/',
-    site: 'https://rdoproject.org/',
-    stats: %w(irc its mediawiki mls qaforums scm)
-  }, {
-    name: 'ManageIQ',
-    summary: "Control applications and workloads across cloud platforms, from one end of a virtual machine's lifecycle to another.",
-    dashboard: 'http://projects.bitergia.com/redhat-manageiq-dashboard/browser/',
-    url: 'http://projects.bitergia.com/redhat-manageiq-dashboard/browser/data/json/',
-    site: 'https://www.manageiq.org/',
-    stats: %w(irc its scm scr qaforums downloads)
-  }, {
-    name: 'Gluster',
-    summary: 'GlusterFS is a scalable network filesystem. Using common off-the-shelf hardware, you can create large, distributed storage solutions for media streaming, data analysis, and other data- and bandwidth-intensive tasks.',
-    dashboard: 'http://projects.bitergia.com/redhat-glusterfs-dashboard/browser/',
-    url: 'http://projects.bitergia.com/redhat-glusterfs-dashboard/browser/data/json/',
-    site: 'https://www.gluster.org/',
-    stats: %w(irc its scm mls downloads)
-  }
-]
+sites = YAML.load_file("dashboard_config.yml")
 
 # Current terminal width, in characters, used for the progressbar
 
@@ -48,6 +24,8 @@ if IO.console
 end
 
 sites.each do |site|
+  site = site.with_indifferent_access
+
   site_name = site[:name]
   uri = URI.parse site[:url]
   # Set progressbar counts
